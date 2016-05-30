7f87316081c24f6e3e8e7fd0d239a2242a3e0da4
KangKai <403155995@qq.com>
2016-05-30 11:22:44 +0800

    自定义html组件

---
+++ jikexueyuan学习示例/angularjs/directive_2.js
@@ -0,0 +1,36 @@
+var app = angular.module('app', []);
+
+// 自定义可重用组件hello
+// app.directive('hello', function() {
+//     return {
+//         restrict: 'E', // element
+//         replace: true, // default: false; 为true时, 替换掉自定义标签的名称
+//         template: '<div>Hello AngularJs</div>'
+//     }
+// })
+
+// app.directive('hello', function() {
+//     return {
+//         link: function() {
+//             alert('我在这里');
+//         }
+//     }
+// })
+
+app.directive('hello', function() {
+    return {
+        restrict: 'C', // class
+        link: function() {
+            alert('class');
+        }
+    }
+})
+
+app.directive('hi', function() {
+    return {
+        restrict: 'A', // attribute
+        link: function() {
+            alert('attribute');
+        }
+    }
+})
