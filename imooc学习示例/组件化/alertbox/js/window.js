cee053821db750a99f63c33b55e57b28c3193caf
KangKai <403155995@qq.com>
2016-06-01 18:18:43 +0800

    调整接口模式

--- imooc学习示例/组件化/alertbox/js/window.js
+++ imooc学习示例/组件化/alertbox/js/window.js
@@ -3,30 +3,32 @@ define(['jquery'], function($) {
     function Window() {
         this.config = {
             width: 500,
-            height:300
+            height:300,
+            content: '',
+            handler: null
         }
     };
 
     Window.prototype = {
-        alert: function(content, handler, config) {
+        alert: function(config) {
+            var Config = $.extend(this.config, config);
             var boundingBox = $('<div class="window_boundingBox"></div>');
             boundingBox.appendTo('body');
-            boundingBox.html(content);
+            boundingBox.html(Config.content);
 
             // 添加按钮和回调入口
             var btn = $('<input type="button" value="确定">');
             btn.appendTo(boundingBox);
             btn.click(function() {
-                handler && handler();
+                Config.handler && Config.handler();
                 boundingBox.remove();
             });
 
-            $.extend(this.config, config);
             boundingBox.css({
-                width: this.config.width + 'px',
-                height: this.config.height + 'px',
-                left: (this.config.x || (window.innerWidth - this.config.width) / 2) + 'px',
-                top: (this.config.y || (window.innerheight - this.config.height) / 2) + 'px',
+                width: Config.width + 'px',
+                height: Config.height + 'px',
+                left: (Config.x || (window.innerWidth - Config.width) / 2) + 'px',
+                top: (Config.y || (window.innerHeight - Config.height) / 2) + 'px',
             })
         },
         confirm: function() {
