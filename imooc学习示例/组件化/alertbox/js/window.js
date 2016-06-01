fa5f79dbc63c9fb6b16058874eda881aceeec81f
KangKai <403155995@qq.com>
2016-06-01 18:28:14 +0800

    定制标题

--- imooc学习示例/组件化/alertbox/js/window.js
+++ imooc学习示例/组件化/alertbox/js/window.js
@@ -1,34 +1,38 @@
 define(['jquery'], function($) {
 
     function Window() {
-        this.config = {
+        this.conf = {
             width: 500,
             height:300,
+            title: '系统消息',
             content: '',
             handler: null
         }
     };
 
     Window.prototype = {
-        alert: function(config) {
-            var Config = $.extend(this.config, config);
-            var boundingBox = $('<div class="window_boundingBox"></div>');
+        alert: function(conf) {
+            var Conf = $.extend(this.conf, conf);
+            var boundingBox = $(
+                '<div class="window_boundingBox">' +
+                    '<div class="window_header">' + Conf.title + '</div>' +
+                    '<div class="window_body">' + Conf.content + '</div>' +
+                    '<div class="window_footer"><input type="button" value="确定"></div>' +
+                '</div>'
+                );
             boundingBox.appendTo('body');
-            boundingBox.html(Config.content);
-
-            // 添加按钮和回调入口
-            var btn = $('<input type="button" value="确定">');
-            btn.appendTo(boundingBox);
+            
+            var btn = boundingBox.find('.window_footer input');
             btn.click(function() {
-                Config.handler && Config.handler();
+                Conf.handler && Conf.handler();
                 boundingBox.remove();
             });
 
             boundingBox.css({
-                width: Config.width + 'px',
-                height: Config.height + 'px',
-                left: (Config.x || (window.innerWidth - Config.width) / 2) + 'px',
-                top: (Config.y || (window.innerHeight - Config.height) / 2) + 'px',
+                width: Conf.width + 'px',
+                height: Conf.height + 'px',
+                left: (Conf.x || (window.innerWidth - Conf.width) / 2) + 'px',
+                top: (Conf.y || (window.innerHeight - Conf.height) / 2) + 'px',
             })
         },
         confirm: function() {
